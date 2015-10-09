a25bbf5dfe17eecef02374394beee10649a08f06
Wenhan Chang <whchang@example.com>
2015-10-09 02:36:13 -0500

    last commit tonight. always segementation fault for results[], even after changing it to vector etc. Going to deal with this next time. nite nite.

--- test.cpp
+++ test.cpp
@@ -18,22 +18,15 @@
 #include <unistd.h>
 
 using namespace std;
-
-int BLOCKLENGTH = 10000; // Block length
-#define maxNumOfHS 3  // Maximum number of crossover hotspots
-// WHC: hard cord to initiate these values
-int crossoverBegin[maxNumOfHS] = {0, 3 * BLOCKLENGTH, 4 * BLOCKLENGTH}; // Start point of crossover regions
-int crossoverEnd[maxNumOfHS] = {3 * BLOCKLENGTH, 4 * BLOCKLENGTH, 5 * BLOCKLENGTH}; // End point of crossover regions
-// WHC: artificially selected values, to represent 2MB region as crossover hotspot
-float crossoverRatio[maxNumOfHS] = {0.15, 0.8, 0.05}; // Relative weights of crossover regions
-int numHS = 3; // WHC: Number of hotspots, single_2 is a scaled hopspot representing a 2MB region
+float * see_return_local_array();
 
 int main() {
-  float total = 0;
-  for (int HS = 0; HS < numHS; HS++) {
-    cout << crossoverRatio[HS] << '\n';
-    total += crossoverRatio[HS];
-  }
-  cout << total << endl;
+
+  float * p = see_return_local_array();
   return 0;
+}
+
+float * see_return_local_array() {
+  static float result[] = {0, 0};
+  return result;
 }
